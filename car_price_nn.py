f10678196d50e4eb0e2dcb54c903972cdee50c3c
Dorian Setnik <dorian.setnik9@gmail.com>
2025-11-29 21:31:54 +0100

    data analysis setup

--- car_price_nn.py
+++ car_price_nn.py
@@ -1,1 +1,8 @@
-print("Hello")
+import pandas as pd
+
+car_data_df = pd.read_csv('./archive/CarPrice_Assignment.csv', engine='pyarrow', dtype_backend='pyarrow')
+
+car_data_df = car_data_df.dropna()
+
+print(car_data_df.info())
+
