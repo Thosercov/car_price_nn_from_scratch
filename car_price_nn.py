17bfc0567383978ef03ef1a234654c22c619cf85
Dorian Setnik <dorian.setnik9@gmail.com>
2025-12-01 00:27:37 +0100

    Converted pyarrow booleans to pandas int type in input data set

--- car_price_nn.py
+++ car_price_nn.py
@@ -1,8 +1,16 @@
 import pandas as pd
-import data_prep as pd
+import numpy as np
+import data_prep as dp
 
-input_data = pd.get_input()
+input_data = dp.get_input()
+
+input_data = input_data.convert_dtypes()
+
+boolean_columns = input_data.select_dtypes(include='bool').columns
+input_data[boolean_columns] = input_data[boolean_columns].astype(int)
 
 print(input_data)
+
+
 
 
