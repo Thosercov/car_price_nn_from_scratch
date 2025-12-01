42e18a1f8fc617da9221cc74ac57e9f1721cd0a9
Dorian Setnik <dorian.setnik9@gmail.com>
2025-12-01 23:12:28 +0100

    Turn categories to dummies one line solution

--- car_price_nn.py
+++ car_price_nn.py
@@ -4,13 +4,4 @@ import data_prep as dp
 
 input_data = dp.get_input()
 
-input_data = input_data.convert_dtypes()
-
-boolean_columns = input_data.select_dtypes(include='bool').columns
-input_data[boolean_columns] = input_data[boolean_columns].astype(int)
-
 print(input_data)
-
-
-
-
