4c305cb112e8a47e53093b0801406d784fdebbaf
Dorian Setnik <dorian.setnik9@gmail.com>
2025-12-07 00:01:55 +0100

    hidden layer added

--- car_price_nn.py
+++ car_price_nn.py
@@ -3,19 +3,30 @@ import numpy as np
 import data_prep as dp
 
 num_of_layers = 3
-inputs = dp.get_inputs()
+num_of_features = 64
+inputs = dp.get_inputs() #pandas turns it into numpy.ndarray
 
 np.random.seed(0)
 
 weights = []
+weights2 = []
 for i in range (0, num_of_layers):
-    weights.append(np.random.uniform(-1, 1, 64))
+    weights.append(np.random.uniform(-1, 1, num_of_features))
+    weights2.append(np.random.uniform(-1, 1, 3))
 
 biases = np.random.randint(-2, 2, 3) # -2, 1, -1
+biases2 = np.random.randint(-2, 2, 3) # 1, -1, -1
 
-layer_outputs = np.dot(inputs, np.array(weights).T) + biases
+weights = np.array(weights)
+weights2 = np.array(weights2)
 
-print(layer_outputs)
-print(layer_outputs.shape) #205, 3
-print(layer_outputs.ndim) #2
+layer1_outputs = np.dot(inputs, weights.T) + biases
+layer2_outputs = np.dot(layer1_outputs, weights2.T) + biases2
+
+print(inputs.values)
+#print(layer1_outputs.shape) #205, 3
+#print(layer1_outputs.ndim) #2
+
+
+
 
