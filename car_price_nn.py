a594f74f73a8cc86260674d1e11b01aa6f6eb018
Dorian Setnik <dorian.setnik9@gmail.com>
2025-12-11 00:11:14 +0100

    added relu activation function class

--- car_price_nn.py
+++ car_price_nn.py
@@ -3,19 +3,21 @@ import numpy as np
 import data_prep as dp
 import constants as c
 from layer import Layer
+from activation_relu import activation_ReLU
 
 if __name__ == '__main__':
     inputs = dp.get_inputs() #pandas turns it into numpy.ndarray
     inputs = inputs.drop_duplicates()
 
-    np.random.seed(0)
+    np.random.seed(3)
 
     layer1 = Layer(c.N_INPUTS, c.N_NEURONS_L1)
 
+    activation1 = activation_ReLU()
+
     layer1.forward(inputs)
 
-    print(layer1.output)
+    activation1.forward(layer1.output)
 
-
-
+    print(activation1.output)
 
