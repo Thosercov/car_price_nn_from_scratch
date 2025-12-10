02fcfdb354c9c0d8f44f4a064db66247be730d07
Dorian Setnik <dorian.setnik9@gmail.com>
2025-12-10 11:26:19 +0100

    For randomization use standard distribution instead of uniform distribution and zeros for biases

--- car_price_nn.py
+++ car_price_nn.py
@@ -8,25 +8,18 @@ inputs = inputs.drop_duplicates()
 
 np.random.seed(0)
 
-weights = []
-weights2 = []
-for i in range (0, c.NUM_OF_NEURONS_L1):
-    weights.append(np.random.uniform(-1, 1, c.NUM_OF_FEATURES))
-    weights2.append(np.random.uniform(-1, 1, c.NUM_OF_NEURONS_L1))
+weights = 0.01 * np.random.randn(inputs.columns.size, c.NUM_OF_NEURONS_L1)
+weights2 = 0.01 * np.random.randn(c.NUM_OF_NEURONS_L1, c.NUM_OF_NEURONS_L2)
 
-biases = np.random.randint(-2, 2, c.NUM_OF_NEURONS_L1) # -2, 1, -1
-biases2 = np.random.randint(-2, 2, c.NUM_OF_NEURONS_L2) # 1, -1, -1
+biases = np.zeros((1, c.NUM_OF_NEURONS_L1))
+biases2 = np.zeros((1, c.NUM_OF_NEURONS_L2))
 
 weights = np.array(weights)
 weights2 = np.array(weights2)
 
-layer1_outputs = np.dot(inputs, weights.T) + biases
-layer2_outputs = np.dot(layer1_outputs, weights2.T) + biases2
+layer1_outputs = np.dot(inputs, weights) + biases
+layer2_outputs = np.dot(layer1_outputs, weights2) + biases2
 
-print(weights)
-
-#print(layer1_outputs.shape) #205, 3
-#print(layer1_outputs.ndim) #2
 
 
 
