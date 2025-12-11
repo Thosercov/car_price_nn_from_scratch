1870121563fa6c2f62b32a04a80ba2bdfadc6ee8
Dorian Setnik <dorian.setnik9@gmail.com>
2025-12-11 23:49:39 +0100

    added activation layer

--- car_price_nn.py
+++ car_price_nn.py
@@ -4,25 +4,27 @@ import data_prep as dp
 import constants as c
 from layer import Layer
 from activation_relu import Activation_ReLU
+from activation_linear import Activation_linear
 from standardization_z_score import Standardization_Z_score
 
 if __name__ == '__main__':
 
     inputs = dp.get_inputs() #pandas turns it into numpy.ndarray
     inputs = inputs.drop_duplicates()
+    standardization_Z_score = Standardization_Z_score()
+    standardized_inputs = standardization_Z_score.standardize(inputs)
 
     np.random.seed(0)
 
     layer1 = Layer(c.N_INPUTS, c.N_NEURONS_L1)
-
     activation1 = Activation_ReLU()
-
-    standardization_Z_score = Standardization_Z_score()
-    standardized_inputs = standardization_Z_score.standardize(inputs)
-
     layer1.forward(standardized_inputs)
-
     activation1.forward(layer1.output)
+
+    layer2 = Layer(c.N_NEURONS_L1, c.N_NEURONS_L2)
+    activation2 = Activation_linear()
+    layer2.forward(activation1.output)
+    activation2.forward(layer2.output)
 
     print(activation1.output)
 
