43535c1089889e3a60a7b7c0161bf05872fe6522
Dorian Setnik <dorian.setnik9@gmail.com>
2025-12-13 15:34:43 +0100

    sine approximation neural network folder - scattered data

---
+++ sine-neural-network/sine_function_nn.py
@@ -0,0 +1,13 @@
+import matplotlib.pyplot as plt
+import numpy as np
+import constants as c
+
+if __name__ == '__main__':
+
+    np.random.seed(0)
+
+    x_samples = np.random.uniform(low = 0.0, high = 2 * np.pi, size = (c.N_SAMPLES, 1))
+    y_samples = np.sin(x_samples) + np.random.normal(loc = 0.0, scale = 0.3, size = (c.N_SAMPLES,1))
+
+    plt.scatter(x_samples, y_samples)
+    plt.show()
