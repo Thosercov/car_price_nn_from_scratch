97a0e8c2c9b6229ea3bd97a3d91f26b5205eade7
Dorian Setnik <dorian.setnik9@gmail.com>
2025-12-14 15:19:24 +0100

    Added 4 layer forward pass

--- sine-neural-network/sine_function_nn.py
+++ sine-neural-network/sine_function_nn.py
@@ -1,6 +1,9 @@
 import matplotlib.pyplot as plt
 import numpy as np
 import constants as c
+from layer import Layer
+from activation_relu import Activation_ReLU
+from activation_linear import Activation_linear
 
 if __name__ == '__main__':
 
@@ -9,5 +12,34 @@ if __name__ == '__main__':
     x_samples = np.random.uniform(low = 0.0, high = 2 * np.pi, size = (c.N_SAMPLES, 1))
     y_samples = np.sin(x_samples) + np.random.normal(loc = 0.0, scale = 0.3, size = (c.N_SAMPLES,1)) #produce some noise on data
 
-    plt.scatter(x_samples, y_samples)
-    plt.show()
+    # layer 1
+    layer1 = Layer(c.N_INPUTS, c.N_LAYER_1_NEURONS)
+    layer1.forward(x_samples)
+
+    activation1 = Activation_ReLU()
+    activation1.forward(layer1.output)
+
+    #layer 2
+    layer2 = Layer(c.N_LAYER_1_NEURONS, c.N_LAYER_2_NEURONS)
+    layer2.forward(activation1.output)
+
+    activation2 = Activation_ReLU()
+    activation2.forward(layer2.output)
+
+    # layer 3
+    layer3 = Layer(c.N_LAYER_2_NEURONS, c.N_LAYER_3_NEURONS)
+    layer3.forward(activation2.output)
+
+    activation3 = Activation_ReLU()
+    activation3.forward(layer3.output)
+
+    # layer 4
+    layer4 = Layer(c.N_LAYER_3_NEURONS, c.N_LAYER_4_NEURONS)
+    layer4.forward(activation3.output)
+
+    activation4 = Activation_ReLU()
+    activation4.forward(layer4.output)
+
+    print(activation4.output)
+
+
